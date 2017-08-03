7660c3a4a523c5e12919342df818657921f5575c
Evan Johnson and Kheifer Fuller <kheifer@>
2017-08-03 08:56:18 -0700

    failed object construction

--- src/main/java/game/BlackJack.java
+++ src/main/java/game/BlackJack.java
@@ -1,7 +1,18 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Guest on 8/3/17.
  */
 public class BlackJack {
+    private String[] suits = {"Spades","Clubs","Hearts","Diamonds"};
+    private String[] values = {"Ace", "2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    private List<String> Deck = new ArrayList<String>();
+
+    public BlackJack() {
+
+
+    }
 }
--- src/test/java/game/BlackJackTest.java
+++ src/test/java/game/BlackJackTest.java
@@ -1,10 +1,18 @@
 package game;
 
+import org.junit.*;
 import static org.junit.Assert.*;
+
 
 /**
  * Created by Guest on 8/3/17.
  */
 public class BlackJackTest {
+    @Test
+    public void runBlackJack_createCardDeck_1(){
+        BlackJack testBlackJack = new BlackJack();
+        assertEquals(false, testBlackJack instanceof BlackJack);
+    }
+
 
 }
